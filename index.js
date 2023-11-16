093a4684774cd870469a5ad8afd13ff1d52f21e7
DivineDemon <supame123@gmail.com>
2023-11-16 18:34:04 +0500

    Fixes!

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import cors from "cors";
+import morgan from "morgan";
 import * as dotenv from "dotenv";
 
 import express from "express";
 import { errorHandler } from "./middleware/errorHandler.js";
-import { default as appInfoRouter } from "./routes/appInfoRoute.js"
-import { default as repoInfoRouter } from "./routes/repoInfoRoute.js"
+import { default as appInfoRouter } from "./routes/appInfoRoute.js";
+import { default as repoInfoRouter } from "./routes/repoInfoRoute.js";
 import { default as experienceRouter } from "./routes/experienceRoute.js";
 
 dotenv.config(); // Configure .env
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 3000; // Initialize Port
 // Middleware
 app.use(cors());
 app.use(errorHandler);
+app.use(morgan(process.env.NODE_ENV));
 app.use(express.json({ limit: "200mb", extended: true }));
 app.use(express.urlencoded({ limit: "200mb", extended: true }));
 
